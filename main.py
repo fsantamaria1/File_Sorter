e0c6b995b1b1509c0643777175cee80dba6e996b
Fredy Santamaria <fsanta3@wgu.edu>
2023-08-08 17:57:13 -0400

    Add the sort_files function.

--- main.py
+++ main.py
@@ -19,3 +19,23 @@ def create_folder(path: str, extension: str) -> str:
         os.makedirs(folder_path)
 
     return folder_path
+
+
+def sort_files(source_path: str):
+    """
+    Sort the files based on their extension.
+
+    :param source_path: Path where the files that need to be sorted are located.
+    """
+
+    for root_dir, sub_dir, filenames in os.walk(source_path):
+        for filename in filenames:
+            file_path: str = os.path.join(root_dir, filename)
+            extension: str = os.path.splitext(filename)[1]
+
+            if extension:
+                target_folder: str = create_folder(source_path, extension)
+                target_path: str = os.path.join(target_folder, filename)
+
+                shutil.move(file_path, target_path)
+
